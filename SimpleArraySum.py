a3583752941fbe616be748b9cdbf7e540bd20023
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-10-22 23:06:07 -0400

    simple algorithm preparation

---
+++ SimpleArraySum.py
@@ -0,0 +1,9 @@
+# Given an array of integers, find the sum of its elements.
+# For example, if the array, ar=[1,2,3], 1 + 2 + 3 = 6, so return 6.
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+# Test cases
+ar = [1, 2, 3, 4, 10, 11]
+print(simpleArraySum(ar))
---
+++ SolveMeFirst.py
@@ -0,0 +1,7 @@
+# Complete the function solveMeFirst to compute the sum of two integers.
+
+def solveMeFirst(a, b): 
+    return a + b
+
+# Test cases
+print(solveMeFirst(2, 3))
